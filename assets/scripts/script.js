2988def65b93d9a1602fd5b32e574fedc62807c1
GMNDS <gabriel@gmnds.com>
2024-11-11 20:32:55 +0000

    configura modelo corretamente, inicia layout para possível documentação

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,8 @@
 const openModalButton = document.querySelector("#open-modal");
 const closeModalButton = document.querySelector("#close-modal");
 const fade = document.querySelector("#fade");
-const modal = document.querySelector("#imprevisto-add");
+const modal = document.querySelector("#modal-add");
+console.log(modal);
 const toggleHide = () => {
   modal.classList.toggle("hide");
   fade.classList.toggle("hide");
