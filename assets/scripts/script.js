cf9bf524980d36e3c05e2689cc6084868491767b
GMNDS <gabriel@gmnds.com>
2024-11-18 05:17:38 +0000

    adiciona modelo aluno com card

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -13,3 +13,14 @@ const toggleHide = () => {
     toggleHide();
   });
 });
+
+const openProfileCard = document.querySelector(".profile-anchor");
+const profileCard = document.querySelector(".profile-dropdown");
+const toggleProfilleActive = () => {
+  profileCard.classList.toggle("active");
+};
+
+openProfileCard.addEventListener("click", () => {
+  console.log("foi");
+  toggleProfilleActive();
+});
