62cdd30d50676645a99888f46aef93df7862feac
GMNDS <gabriel@gmnds.com>
2024-11-21 18:42:14 -0300

    adiciona landing page e organiza arquivos

--- src/assets/scripts/login.js
+++ src/assets/scripts/login.js
@@ -1,6 +1,8 @@
 const optionsLogin = document.querySelector(".login-options");
-// const optionLogin = document.querySelector(".login-option-link.login");
-// const optionLoginFirst = document.querySelector(".login-option-link.first");
+const firstAcess = document.querySelector(".login-option-link.first-acess");
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
+const sendAuth = document.querySelector("#send-auth");
 
 const toggleOptions = (event) => {
   const target = event.target;
@@ -9,9 +11,33 @@ const toggleOptions = (event) => {
       element.classList.remove("option-active");
     });
     target.classList.toggle("option-active");
+
+    if (target.classList.contains("first-acess")) {
+      username.placeholder = "Insira seu RA";
+      password.placeholder = "Crie sua senha";
+    } else {
+      username.placeholder = "Insira seu RA ou e-mail";
+      password.placeholder = "Insira sua senha";
+    }
   }
 };
 
 optionsLogin.addEventListener("click", toggleOptions);
-// optionLogin.addEventListener("click", toggleOptions);
-// optionLoginFirst.addEventListener("click", toggleOptions);
+
+sendAuth.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  switch (username.value + password.value) {
+    case "admin1234":
+      window.location.href = "/goe";
+      break;
+    case "aluno1234":
+      window.location.href = "/aluno";
+      break;
+    case "professor1234":
+      window.location.href = "/professor";
+      break;
+    default:
+      alert("Usuário ou senha inválidos");
+  }
+});
